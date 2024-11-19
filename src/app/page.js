ab3c6d472a620257f667e508c946b2b1e9f7ffd0
CRAZYROCK04 <crazytoysarg@gmail.com>
2024-11-19 19:26:14 -0300

    subiendo archivos

---
+++ src/app/page.js
@@ -0,0 +1,48 @@
+"use client";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
+export default function Home() {
+  const [reservations, setReservations] = useState([]);
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+
+  useEffect (() => {
+    const savedReservations = JSON.parse (localStorage.getItem ("reservations")) || [];
+    setReservations (savedReservations);
+  },[]);
+
+  const addReservation = () => {
+    const newReservation = {
+      id: reservations.length + 1,
+      name,
+      date,
+    };
+    const updatedReservations = [...reservations,newReservation]
+    setReservations(updatedReservations);
+    localStorage.setItem ("reservations", JSON.stringify (updatedReservations));
+    setName("");
+    setDate("");
+  };
+
+  return (
+    <div>
+      <h1>Sistema de reservas</h1>
+      <h2>Crear una nueva reserva</h2>
+      <input type="text" placeholder="nombre" value={name} onChange={(e) => setName(e.target.value)}></input>
+      <input type="date" value={date} onChange={(e) => setDate(e.target.value)}></input>
+      <button onClick={addReservation}>Añadir reserva</button>
+
+      <h2>Reservas activas</h2>
+      <ul>
+        {reservations.map((res) => (
+          <li key={res.id}>
+            <Link href={`/reservation/${res.id}`}>
+              {res.name} - {res.date}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ src/app/reservation/[id].js
@@ -0,0 +1,66 @@
+"use client";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+
+export default function reservationDetail() {
+    const router = useRouter();
+    const { id } = router.query;
+    const [reservation, setReservation] = useState(null);
+    const [isEditing, setIsEditing] = useState(false);
+    const [name, setName] = useState("");
+    const [date, setDate] = useState("");
+
+    useEffect(() => {
+        const savedReservations = JSON.parse(localStorage.getItem("reservations")) || [];
+        const foundReservation = savedReservations.find(res => res.id !== parseInt(id));
+        if (foundReservation) {
+            setReservation(foundReservation);
+            setName(foundReservation.name);
+            setDate(foundReservation.date);
+        }
+    }, [id]);
+
+    const cancelReservation = () => {
+        const savedReservations = JSON.parse(localStorage.getItem("reservations")) || [];
+        const updatedReservations = savedReservations.filter(res => res.id !== parseInt(id));
+        localStorage.setItem("reservations", JSON.stringify(updatedReservations));
+        router.push("/");
+    };
+
+    const toggleEditMode = () => {
+        setIsEditing(!isEditing);
+    };
+
+    const saveEdit = () => {
+        const savedReservations = JSON.parse(localStorage.getItem("reservations")) || [];
+        const updatedReservations = savedReservations.map(res => res.id === reservation.id ? { ...res, name, date } : res);
+        localStorage.setItem("reservations", JSON.stringify(updatedReservations));
+        setReservation({ ...reservation, name, date });
+        setIsEditing(false);
+    };
+
+    if (!reservation) return <p>Cargando</p>;
+
+    return (
+        <div>
+            <h1>Detalles Reserva</h1>
+            {isEditing ? (
+                <div>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
+                    <input type="date" value={date} onChange={(e) => setDate(e.target.value)}></input>
+                    <button onClick={saveEdit}>Guardar</button>
+                    <button onClick={toggleEditMode}>Cancelar</button>
+                </div>
+
+            ) : (
+                <div>
+                    <p>Nombre: {reservation.name}</p>
+                    <p>Fecha: {reservation.date}</p>
+                    <button onClick={toggleEditMode}>Editar Reserva</button>
+                    <button onClick={cancelReservation}>Cancelar Reserva</button>
+                </div>
+            )}
+
+        </div>
+    );
+}
